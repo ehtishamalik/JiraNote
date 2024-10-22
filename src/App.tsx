ce27fb5de674b14aed60442b4d54b5529f0bccaf
ehtishamalik <malikehtishamali334@hotmail.com>
2024-10-22 22:00:35 +0500

    Initial structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './styles/index.scss';
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
