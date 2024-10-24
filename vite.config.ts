277757b791cd008687e45b4a0b1bcf3371c60c14
ehtishamalik <malikehtishamali334@hotmail.com>
2024-10-24 11:39:08 +0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs', // Change output folder to 'docs'
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler'
-      }
-    }
-  }
-})
+        api: 'modern-compiler',
+      },
+    },
+  },
+});
